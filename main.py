f5ebb07c523fc4e6a15490dc3a1e76009a1b0642
sathishO2 <20btad036@gmail.com>
2024-11-05 22:04:14 +0530

    day 1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import (Optional, Dict,)
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,7 +12,48 @@ def read_root():
     }
 
 @app.get("/greet/")
-def greet(username:str):
+def greet(username: Optional[str] = "Your Name!"):
     return {
         "message":f"Hello, {username}"
     }
+
+phone_list = [
+    "samsung",
+    "apple",
+    "vivo",
+    "oppo",
+    "mi"
+]
+
+@app.get("/search")
+def search_for_phone(brand:str):
+    if brand in phone_list:
+        return {
+            "message": f"{brand} is available"
+        }
+    else:
+        return {
+            "message": f"{brand} is not available"
+        }
+
+user_db:Dict[str, str] = {}
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+
+@app.post("/create_user")
+def create_user(payload: CreateUserRequest):
+
+    if payload.email in user_db:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already exists"
+        )
+    user_db[payload.email] = payload.username
+
+    return {
+        "message": "User Created Successfully",
+        "user": payload
+    }
+
