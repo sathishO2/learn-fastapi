ae64031609bf801dec6a09a8adb0cb60dab86950
sathishO2 <20btad036@gmail.com>
2024-11-05 20:50:43 +0530

    start learning...

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {
+        "message":"hello world"
+    }
+
+@app.get("/greet/")
+def greet(username:str):
+    return {
+        "message":f"Hello, {username}"
+    }
