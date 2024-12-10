21235373a462bcef6c3526826c8084270c4e16a8
sathishO2 <20btad036@gmail.com>
2024-12-10 08:46:03 +0530

    minor change

--- src/auth/models.py
+++ src/auth/models.py
@@ -21,6 +21,9 @@ class User(SQLModel, table=True):
     username: str
     first_name: str = Field(nullable=True)
     last_name: str = Field(nullable=True)
+    role: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False, server_default="user")
+    )
     is_verified: bool = False
     email: str
     password_hash: str
